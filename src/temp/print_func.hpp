59048563c07400475b3e2cd7c4ec9aa69467204c
lePici <warband.times@gmail.com>
2020-09-25 18:21:39 +0200

    tree printer MAINTENANCE remove comments

--- src/temp/print_func.hpp
+++ src/temp/print_func.hpp
@@ -35,7 +35,6 @@ class Out
     {
         os << string("\"");
         for(auto const& item: out.data)
-            //os << item << string("-");
             os << item;
         os << string("\"");
         return os;
@@ -96,39 +95,5 @@ void print(void* out, int arg_count, va_list ap)
 }
 
 }
-    //using namespace Print_func;
-
-    //std::vector<std::string> out;
-    //const char *space = " ";
-    //wrapper(&out, 1, space);
-    //print_vec(out); out.clear();
-
-    //wrapper(&out, 2, space, "lol");
-    //print_vec(out); out.clear();
-
-    //wrapper(&out, 3, space, "lol", space);
-    //print_vec(out); out.clear();
-
-    //wrapper(&out, 3, space,  space, "lol");
-    //print_vec(out); out.clear();
-
-    //wrapper(&out, 3, "lol",  space, space);
-    //print_vec(out); out.clear();
-
-//void wrapper(void* out, int arg_count, ...)
-//{
-//    va_list ap;
-//    va_start(ap, arg_count);
-//    print_func(out, arg_count, ap);
-//    va_end(ap);
-//}
-//
-//void print_vec(vector<string> out)
-//{
-//    for(auto item: out)
-//        std::cout << item << "|";
-//    std::cout << std::endl;
-//}
-
 
 #endif
